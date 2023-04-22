c1aedd91d68c7a3508ad13e9e905af40202d94fb
rimbo537 <izuravel524@gmail.com>
2023-04-22 13:11:30 +0300

    first commit

---
+++ lib/resources/app_images.dart
@@ -0,0 +1,4 @@
+abstract class AppImages {
+  static const String river = 'assets/images/river.jpg';
+  static const String nature = 'assets/images/nature.jpg';
+}
---
+++ lib/resources/app_strings.dart
@@ -0,0 +1,4 @@
+abstract class AppStrings {
+  static const String imageComparison = 'Image Comparison';
+  static const String dragToSeeChanges = 'Drag to see changes';
+}
